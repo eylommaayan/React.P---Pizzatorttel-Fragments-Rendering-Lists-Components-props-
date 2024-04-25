e671371d62319334dce693ce825cda688176bac5
EylonMaayan <56929518+eylommaayan@users.noreply.github.com>
2024-04-25 17:53:38 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDom from "react-dom/client";
+
+function App() {
+  return (
+    <>
+      <h1> שלום צבי הניניג'ה</h1>
+    </>
+  );
+}
+
+export default App;
